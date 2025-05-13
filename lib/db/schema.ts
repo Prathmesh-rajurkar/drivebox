1d835bc214c884599c62a03927891c918cfdf899
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-05-14 05:00:03 +0530

    orm and db

---
+++ lib/db/schema.ts
@@ -0,0 +1,34 @@
+import {
+  pgTable,
+  serial,
+  text,
+  uuid,
+  integer,
+  boolean,
+  varchar,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import path from "path";
+
+export const files = pgTable("files", {
+  id: uuid("id").defaultRandom().primaryKey(),
+
+  name: text("name").notNull(),
+  path: text("path").notNull(),
+  size: integer("size").notNull(),
+  type: text("type").notNull(),
+
+  fileurl: text("fileurl").notNull(),
+  thumbnailurl: text("thumbnailurl"),
+
+  userId: text("userId").notNull(),
+  parentId: uuid("parentId"),
+
+  isFolder: boolean("isFolder").default(false).notNull(),
+  isStared: boolean("isStared").default(false).notNull(),
+  isTrash: boolean("isTrash").default(false).notNull(),
+
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+  updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+});
