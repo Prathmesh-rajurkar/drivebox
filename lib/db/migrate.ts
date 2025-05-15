a2c66e0b6de1807839bfce42cb5ead5559959e68
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-05-15 12:41:16 +0530

    zod

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -2,7 +2,6 @@ import { migrate } from "drizzle-orm/neon-http/migrator";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as dotenv from "dotenv";
-import { log } from "console";
 
 dotenv.config({ path: ".env" });
 
@@ -11,19 +10,17 @@ if (!process.env.DATABASE_URL) {
 }
 
 async function runMigration() {
-    try {
-        const sql = neon(process.env.DATABASE_URL!);
-        const db = drizzle(sql);
-        await migrate(db, {
-            migrationsFolder: "./drizzle",
-            migrationsTable: "__drizzle_migration",
-            migrationsSchema: "public",
-        });
-        console.log("Migration completed successfully");
-    } catch (error) {
-        console.error("Migration failed:", error);
-        process.exit(1)
-    }
+  try {
+    const sql = neon(process.env.DATABASE_URL!);
+    const db = drizzle(sql);
+    await migrate(db, {
+      migrationsFolder: "./drizzle",
+    });
+    console.log("Migration completed successfully");
+  } catch (error) {
+    console.error("Migration failed:", error);
+    process.exit(1);
+  }
 }
 
-runMigration()
+runMigration();
